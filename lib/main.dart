cf0af2d89bf732840f96d12246d8879a780c3ee3
izu <a.iz.dnlv.335@gmail.com>
2023-06-14 15:21:16 +0900

    縦画面固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  //向き指定
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, //縦固定
+  ]);
   runApp(MyLayoutApp());
 }
 
